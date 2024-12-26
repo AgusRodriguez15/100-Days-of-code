c88d48fe5b4d89390e1c99c49ce0280ef3c597f1
AgusRodriguez15 <8670@holbertonstudents.com>
2024-12-26 19:43:56 -0300

    name generator

---
+++ Day_1/task_6.py
@@ -0,0 +1,12 @@
+#1.Create a greeting for your program.
+#2.Ask the user for the city that they grew up in and store it in a variable.
+#3.Ask the user for the name of a pet and store it in a variable.
+#4.Combine the name of their city and pet and show them their band name.
+print("hello friend, nice to see you here")
+print("What is the name of the city that you grow up?")
+cityName = input()
+print(cityName)
+print("What is the name of your pet")
+petName = input()
+print(petName)
+print("your band name is: ", cityName + " " + petName)
